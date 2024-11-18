0fcec8ac734afa02bfa215aebb707b0bfa0dedf1
jeisonlopez888 <162076804+jeisonlopez888@users.noreply.github.com>
2024-11-18 00:58:55 -0500

    pruebas

--- src/test/java/co/edu/uniquindio/poo/AdministradorTest.java
+++ src/test/java/co/edu/uniquindio/poo/AdministradorTest.java
@@ -1,6 +1,6 @@
 package co.edu.uniquindio.poo;
 
-import org.junit.jupiter.api.Test;
+
 import static org.junit.jupiter.api.Assertions.fail;
 import java.util.logging.Logger;
 
---
+++ src/test/java/co/edu/uniquindio/poo/ClienteTest.java
@@ -0,0 +1,29 @@
+package co.edu.uniquindio.poo;
+
+import java.util.logging.Logger;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+    
+public class ClienteTest {
+
+    @Test
+    public void testClienteGettersAndSetters() {
+        // Crear una instancia de Cliente
+        Cliente cliente = new Cliente();
+
+        // Establecer valores utilizando los setters
+        cliente.setNombre("Juan Pérez");
+        cliente.setDocumento("123456789");
+        cliente.setTelefono("555-1234");
+        cliente.setDireccion("Calle Falsa 123");
+
+        // Verificar que los valores se establecieron correctamente utilizando los getters
+        assertEquals("Juan Pérez", cliente.getNombre());
+        assertEquals("123456789", cliente.getDocumento());
+        assertEquals("555-1234", cliente.getTelefono());
+        assertEquals("Calle Falsa 123", cliente.getDireccion());
+    }
+
+    
+}
